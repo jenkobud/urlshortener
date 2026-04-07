8800823ab9f75802fafbcb8b8817f76f5366f7db
Jenko <jbudnechky@fi.uba.ar>
2026-04-07 17:10:56 -0300

    docs: add OpenAPI schema annotations to UrlShortenerRequest

--- src/main/java/com/roadmapsh/urlshortener/dtos/requests/UrlShortenerRequest.java
+++ src/main/java/com/roadmapsh/urlshortener/dtos/requests/UrlShortenerRequest.java
@@ -1,5 +1,6 @@
 package com.roadmapsh.urlshortener.dtos.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -9,8 +10,10 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Schema(description = "Request body for creating or updating a short URL")
 public class UrlShortenerRequest {
     @NotNull
     @NotEmpty
+    @Schema(description = "The URL to shorten", example = "https://www.example.com/some/long/url")
     private String url;
 }
